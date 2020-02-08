88cacae6152d24b9bd8db73e96d94012335dd00c
wbelden <60723838+wbelden@users.noreply.github.com>
2020-02-07 22:11:06 -0600

    Update Adventure.cpp

--- Adventure.cpp
+++ Adventure.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     
- \\Debug mode
+ //Debug mode
     bool debug = false;
     int choice;
     cout << "Choose play mode: \n";
@@ -29,7 +29,7 @@ int main()
         break;
     }
     
- \\introduction & variables
+ //introduction & variables
     srand(static_cast<unsigned int>(time(0)));
     cout << "Hello defender!\nWhat is your name?\n";
     string defender;
@@ -42,25 +42,25 @@ int main()
     int turns = 0;
     int curr_round = 0;
     
- \\make max round random
+ //make max round random 
     int max_round = (rand() % 3) + 6;
     if (debug == 1)
     {
     cout << "Max round: " << max_round << endl;
     }
     
- \\fight loop
+ //fight loop
     while (health > 0 && curr_round != max_round)
     {
-     \\increase round and state round
+     //increase round and state round
         ++curr_round;
-        cout << max_round - curr_round + 1 << "day(s) till help arrives...\n";
+        cout << endl << max_round - curr_round + 1 << " day(s) till help arrives...\n";
         if (debug == 1)
         {
         cout << "Round: " << curr_round << endl;
         }
         
-     \\attack & defense rolls
+     //attack & defense rolls
         attack = rand() % 5;
         block = rand() % 5;
         if (debug == 1)
@@ -69,42 +69,42 @@ int main()
         cout << "Block: " << block << endl;
         }
         
-     \\calculate damage and state block or hit
+     //calculate damage and state block or hit
         hit = attack - block;
         if (hit > 0)
         {
             health -= hit;
-            cout << "You were hit for " << hit << " damage!\n\n";
+            cout << "You were hit for " << hit << " damage!\n";
         }
         else
         {
-            cout << "You succesfully blocked the attack!\n\n";
+            cout << "You succesfully blocked the attack!\n";
         }
         if(debug == 1)
         {
         cout << "Health: " << health << endl;
         }
         
-     \\low health warning
+     //low health warning
         if (health == 3 || health == 4)
         {
             cout << "Please, " << defender << ", you must rest soon!\n";
         }
         
-     \\rest for two seconds
+     //rest for two seconds
         this_thread::sleep_for(chrono::milliseconds(2000));
 
         continue;
     }
     
- \\winning or losing statements
+ //winning or losing statements
     if (health <= 0) 
     {
-        cout << defender << ", your body has failed you...\njust as you have failed your people.\n";
+        cout << endl << defender << ", your body has failed you...\njust as you have failed your people.\n";
     }
     else
     {
-        cout << defender << ", you have held out long enough for help to arrive.\nYour victory will be praised as long as your home stands!\n";    
+        cout << endl << defender << ", you have held out long enough for help to arrive.\nYour victory will be praised as long as your home stands!\n";    
     }
     
     return 0;
